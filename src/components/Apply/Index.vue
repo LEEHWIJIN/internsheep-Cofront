9e502f80b666c01644c231530dc90497b24fa216
chj777 <chj777@ajou.ac.kr>
2019-04-29 20:17:49 +0900

    Connect DB and write apply

--- src/components/Apply/Index.vue
+++ src/components/Apply/Index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="Apply">
-    <form v-on:submit.prevent='submitResume'>
+  <div id="apply">
+    <form v-on:submit.prevent='submitNotice'>
       <label>회사명*</label><br>
       <input v-model="cName" placeholder="회사명울 입력 해주세요."><br>
       <label>담당자 이름*</label><br>
@@ -9,8 +9,8 @@
       <input v-model="cLocation" placeholder="회사 주소를 입력 해주세요"><br>
       <label>복리후생*</label><br>
       <input v-model="cBenefit" placeholder="복리후생 정보를 입력 해주세요"><br>
-      <label>학년정보(현재 학기 기준)*</label><br>
-      <input v-model="cPay" placeholder="복리후생 정보를 입력 해주세요"><br>
+      <label>급여*</label><br>
+      <input v-model="cPay" placeholder="급여 정보를 입력 해주세요"><br>
       <label>실습시작일*</label><br>
       <input v-model="InternTermStart" placeholder="실습시작일을 입력 해주세요"><br>
       <label>활동부서*</label><br>
@@ -47,14 +47,14 @@
 
       },
       methods: {
-        // submitResume(){
-        //   this.$http.post('http://localhost:8888/std/resume',{sNum:this.sNum,sName: this.sName}).then((response) => {
-        //       console.log(this.sName)
-        //       console.log(this.sNum)
-        //       this.sName = "";
-        //       this.sNum="";
-        //     })
-        // },
+         submitNotice(){
+           this.$http.post('http://localhost:8888/co/mypage/writeNotice',{cName:this.cName,cManagerName: this.cManagerName}).then((response) => {
+               console.log(this.cName)
+               console.log(this.cManagerName)
+               this.cName = "";
+               this.cManagerName="";
+             })
+         },
       }
   }
 </script>
