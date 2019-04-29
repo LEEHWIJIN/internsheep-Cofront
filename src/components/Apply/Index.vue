46db3abcad8c4a208ff4f63a4a662440242c4052
chj777 <chj777@ajou.ac.kr>
2019-04-29 19:24:42 +0900

    Add dev branch

--- src/components/Apply/Index.vue
+++ src/components/Apply/Index.vue
@@ -1,29 +1,6 @@
 <template>
   <div id="Apply">
     <form v-on:submit.prevent='submitResume'>
-      <label>이메일 주소*</label><br>
-      <input v-model="email" placeholder="이메일 주소"><br>
-      <label>이름*</label><br>
-      <input v-model="sName" placeholder="이름을 입력 해주세요"><br>
-      <label>소속학과*</label><br>
-      <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
-      <label for="jack">소프트웨어학과</label><br>
-      <input type="checkbox" id="john" value="John" v-model="checkedNames">
-      <label for="john">ICT융합전공</label><br>
-      <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
-      <label for="mike">기타</label><br>
-      <label>학번*</label><br>
-      <input v-model="sNum" placeholder="학번을 입력 해주세요"><br>
-      <label>전화번호*</label><br>
-      <input v-model="email" placeholder="전화번호를 입력 해주세요"><br>
-      <label>학년정보(현재 학기 기준)*</label><br>
-      <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
-      <label for="jack">3학년 1학기</label><br>
-      <input type="checkbox" id="john" value="John" v-model="checkedNames">
-      <label for="john">3학년 2학기</label><br>
-      <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
-      <label for="mike">4학년 1학기</label><br>
-      <input class="hello" type="submit" value="확인">
     </form>
   </div>
 </template>
@@ -41,7 +18,7 @@
         }
       },
       components: {
-          
+
       },
       created(){
 
