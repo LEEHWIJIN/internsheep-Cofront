e84393a669275c8339c172129d834d3da536404a
chj777 <chj777@ajou.ac.kr>
2019-05-18 17:21:48 +0900

    Modal modifying

--- src/components/ViewAppliedStudents/Index.vue
+++ src/components/ViewAppliedStudents/Index.vue
@@ -17,14 +17,13 @@
             <p class="my-4">Hello from modal!</p>
           </b-modal>
           <div>
-            <button @click="handleClickButton">Toggle Modal</button>
-            <app-my-modal
-              title="This is modal"
-              :visible.sync="visible">
-              <div>
-                This is modal body
-              </div>
-            </app-my-modal>
+            <b-button v-b-modal.modal-tall>Launch overflowing modal</b-button>
+            <b-modal id="modal-tall" title="Overflowing Content">
+              <p class="my-4" v-for="i in 20" :key="i">
+                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+              </p>
+            </b-modal>
           </div>
         </div>
       </div>
