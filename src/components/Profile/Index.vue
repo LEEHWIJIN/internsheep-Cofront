bedfed170ad585db4f502607bf649cd8e033c56c
chj777 <chj777@ajou.ac.kr>
2019-05-30 20:35:02 +0900

    View applied Student Specification Button Modified

--- src/components/Profile/Index.vue
+++ src/components/Profile/Index.vue
@@ -1,59 +1,59 @@
 <!-- 공고 작성 페이지  -->
 <template>
-<section class="section section-lg-bottom bg-light">
-  <div  class="container" id="apply">
-    <v-base></v-base>
-    <div class="row">
-    <!-- 카테고리란 -->
-    <div class="col-lg-3">
-      <v-category></v-category>
-    </div>
-    <!-- 공백 -->
-    <div class="col-lg-1">
-    </div>
-    <!-- 회원 정보 -->
-    <div class="col-lg-8">
+  <section class="section section-lg-bottom bg-light">
+    <div  class="container" id="apply">
+      <v-base></v-base>
       <div class="row">
-        <div class="col-lg-12 text-center">
-          <p class="subtitle">Notice</p>
-          <h2 class="section-title">Write a Notice</h2>
+      <!-- 카테고리란 -->
+        <div class="col-lg-3">
+          <v-category></v-category>
         </div>
-        <div class="col-lg-12 text-center p-0">
-        <form class="row" v-on:submit.prevent='submitNotice' >
-          <div class="col-lg-6">
-            <input class="form-control mb-4" v-model="cName" placeholder="회사명">
+        <!-- 공백 -->
+        <div class="col-lg-1">
+        </div>
+        <!-- 회원 정보 -->
+        <div class="col-lg-8">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <p class="subtitle">Notice</p>
+              <h2 class="section-title">Write a Notice</h2>
+            </div>
+            <div class="col-lg-12 text-center p-0">
+            <form class="row" v-on:submit.prevent='submitNotice' >
+              <div class="col-lg-6">
+                <input class="form-control mb-4" v-model="cName" placeholder="회사명">
+              </div>
+              <div class="col-lg-6">
+                <input class="form-control mb-4" v-model="cCeoName" placeholder="대표명">
+              </div>
+              <div class="col-lg-6">
+                <input class="form-control mb-4" v-model="cCompanyNum" placeholder="사업자등록번호">
+              </div>
+              <div class="col-lg-6">
+                <input class="form-control mb-4" v-model="cCeoPhone" placeholder="대표 전화번호">
+              </div>
+              <div class="col-lg-6">
+                <input class="form-control mb-4" v-model="cManagerName" placeholder="담당자명">
+              </div>
+              <div class="col-lg-6">
+                <input class="form-control mb-4" v-model="cManagerPhone" placeholder="담당자 전화번호">
+              </div>
+              <div class="col-lg-6">
+                <input class="form-control mb-4" v-model="cPassword" placeholder="비밀번호">
+              </div>
+              <div class="col-lg-12">
+                <input class="form-control mb-4" v-model="cLocation" placeholder="회사 주소">
+              </div>
+              <div class="col-12">
+                <button class="btn btn-primary" type="submit">Submit Now</button>
+              </div>
+            </form>
           </div>
-          <div class="col-lg-6">
-            <input class="form-control mb-4" v-model="cCeoName" placeholder="대표명">
           </div>
-          <div class="col-lg-6">
-            <input class="form-control mb-4" v-model="cCompanyNum" placeholder="사업자등록번호">
-          </div>
-          <div class="col-lg-6">
-            <input class="form-control mb-4" v-model="cCeoPhone" placeholder="대표 전화번호">
-          </div>
-          <div class="col-lg-6">
-            <input class="form-control mb-4" v-model="cManagerName" placeholder="담당자명">
-          </div>
-          <div class="col-lg-6">
-            <input class="form-control mb-4" v-model="cManagerPhone" placeholder="담당자 전화번호">
-          </div>
-          <div class="col-lg-6">
-            <input class="form-control mb-4" v-model="cPassword" placeholder="비밀번호">
-          </div>
-          <div class="col-lg-12">
-            <input class="form-control mb-4" v-model="cLocation" placeholder="회사 주소">
-          </div>
-          <div class="col-12">
-            <button class="btn btn-primary" type="submit">Submit Now</button>
-          </div>
-        </form>
-      </div>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
-</section>
+  </section>
 </template>
 
 <script>
--- src/components/ViewAppliedStudents/Index.vue
+++ src/components/ViewAppliedStudents/Index.vue
@@ -10,58 +10,6 @@
       </div>
       <!-- 공백 -->
       <div class="col-lg-1">
-        <!-- 상세 정보 버튼 -->
-
-        <!--<div>-->
-          <!--&lt;!&ndash;&lt;!&ndash; 긴 모달 &ndash;&gt;&ndash;&gt;-->
-          <!--&lt;!&ndash;<div>&ndash;&gt;-->
-            <!--&lt;!&ndash;<b-button v-b-modal.modal-tall>이력서 보기</b-button>&ndash;&gt;-->
-            <!--&lt;!&ndash;<b-modal id="modal-tall" title="이력서 세부내용">&ndash;&gt;-->
-               <!--&lt;!&ndash;<p class="my-1" v-for="i in 20" :key="i">&ndash;&gt;-->
-                <!--&lt;!&ndash;Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis&ndash;&gt;-->
-                <!--&lt;!&ndash;in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.&ndash;&gt;-->
-              <!--&lt;!&ndash;</p>&ndash;&gt;-->
-              <!--&lt;!&ndash;<div class="row">&ndash;&gt;-->
-                <!--&lt;!&ndash;<div class="col-lg-12 text-center">&ndash;&gt;-->
-                  <!--&lt;!&ndash;<img src="images/career/logo-5.png" class="img shadow-sm mr-sm-3 m-3 p-0" alt="logo-1">&ndash;&gt;-->
-                <!--&lt;!&ndash;</div>&ndash;&gt;-->
-              <!--&lt;!&ndash;</div>&ndash;&gt;-->
-              <!--&lt;!&ndash;<p class="my-1">&ndash;&gt;-->
-                <!--&lt;!&ndash;이름:&ndash;&gt;-->
-              <!--&lt;!&ndash;</p>&ndash;&gt;-->
-              <!--&lt;!&ndash;<p class="my-1">&ndash;&gt;-->
-                <!--&lt;!&ndash;소속학과:&ndash;&gt;-->
-              <!--&lt;!&ndash;</p>&ndash;&gt;-->
-              <!--&lt;!&ndash;<p class="my-1">&ndash;&gt;-->
-                <!--&lt;!&ndash;학번:&ndash;&gt;-->
-              <!--&lt;!&ndash;</p>&ndash;&gt;-->
-              <!--&lt;!&ndash;<p class="my-1">&ndash;&gt;-->
-                <!--&lt;!&ndash;전화번호:&ndash;&gt;-->
-              <!--&lt;!&ndash;</p>&ndash;&gt;-->
-              <!--&lt;!&ndash;<p class="my-1">&ndash;&gt;-->
-                <!--&lt;!&ndash;E-mail:&ndash;&gt;-->
-              <!--&lt;!&ndash;</p>&ndash;&gt;-->
-
-            <!--&lt;!&ndash;</b-modal>&ndash;&gt;-->
-          <!--&lt;!&ndash;</div>&ndash;&gt;-->
-          <!--&lt;!&ndash; 짧은 모달 &ndash;&gt;-->
-          <!--<b-button v-b-modal.modal-1>Launch demo modal</b-button>-->
-          <!--<b-modal id="modal-1" title="BootstrapVue">-->
-            <!--<p class="my-1">Hello from modal!</p>-->
-          <!--</b-modal>-->
-          <!--&lt;!&ndash; 몬생긴 모달 &ndash;&gt;-->
-          <!--<div>-->
-            <!--<button @click="handleClickButton">Toggle Modal</button>-->
-            <!--<app-my-modal-->
-              <!--title="This is modal"-->
-              <!--:visible.sync="visible">-->
-              <!--<div>-->
-                <!--This is modal body-->
-              <!--</div>-->
-            <!--</app-my-modal>-->
-          <!--</div>-->
-        <!--</div>-->
-
       </div>
       <!-- 지원 현황 -->
       <div class="col-lg-8">
@@ -70,37 +18,60 @@
             <p class="subtitle">Students</p>
             <h2 class="section-title">Applied Students</h2>
           </div>
-          <div v-if="$store.state.apply.apply_state==0">
-          <div v-for='(sl,index) in stdList' class="col-lg-12 bg-white p-4 rounded shadow my-3">
-            <div class="media align-items-center flex-column flex-sm-row">
-              <!-- 학생 사진 -->
-              <img src="images/career/logo-5.png" class="mr-sm-3 mb-4 mb-sm-0 border rounded p-2" alt="logo-1">
-              <!-- 학생 간단 정보 -->
-              <div class="media-body text-center text-sm-left mb-4 mb-sm-0">
-                <h6 class="mt-0">{{sl.sName}} - Team of PHP MySQL Developers </h6>
-                <p class="mb-0 text-gray">{{sl.sMajor}} | {{sl.sGrade}} | 학점 3.3</p>
+          <div class="col-lg-12 text-center">
+            <div v-if="$store.state.apply.apply_state==0">
+              <div v-for='(sl,index) in stdList' class="col-lg-12 bg-white p-4 rounded shadow my-3">
+                <div class="media align-items-center flex-column flex-sm-row">
+                  <!-- 학생 사진 -->
+                  <img src="images/career/logo-5.png" class="mr-sm-3 mb-4 mb-sm-0 border rounded p-2" alt="logo-1">
+                  <!-- 학생 간단 정보 -->
+                  <div class="media-body text-center text-sm-left mb-4 mb-sm-0">
+                    <h6 class="mt-0">{{sl.sName}} - Team of PHP MySQL Developers </h6>
+                    <p class="mb-0 text-gray">{{sl.sMajor}} | {{sl.sGrade}} | 학점 3.3</p>
+                  </div>
+                  <div>
+                    <select @change="ynchange(index,$event)">
+                      <option value="" selected disabled hidden style="font-color:#aab1bb;">합격여부</option>
+                      <option value="1">합격</option>
+                      <option value="0">불합격</option>
+                    </select>
+                  </div>
+                  <div>
+                    <b-button v-b-modal="modalId(index)">이력서 보기</b-button>
+                    <b-modal :id="'modal'+index" title="이력서 세부내용">
+                      <div class="row">
+                        <div class="col-lg-12 text-center">
+                          <img src="images/career/logo-5.png" class="img shadow-sm mr-sm-3 m-3 p-0" alt="logo-1">
+                        </div>
+                      </div>
+                      <p class="my-1">
+                        이름:
+                      </p>
+                      <p class="my-1">
+                        소속학과:
+                      </p>
+                      <p class="my-1">
+                        학번:
+                      </p>
+                      <p class="my-1">
+                        전화번호:
+                        </p>
+                      <p class="my-1">
+                        E-mail:
+                      </p>
+                    </b-modal>
+                  </div>
+                  <!-- 학생 상세 정보 버튼 -->
+                  <!-- <a href="#" @click="finishJudge(index,sl.selected)" class="btn btn-outline-primary">심사 완료</a> -->
+                </div>
               </div>
-              <div>
-              <select @change="ynchange(index,$event)">
-                <option value="" selected disabled hidden style="font-color:#aab1bb;">합격여부</option>
-                <option value="1">합격</option>
-                <option value="0">불합격</option>
-              </select>
-              </div>
-              <!-- 학생 상세 정보 버튼 -->
-              <!-- <a href="#" class="btn btn-outline-primary">Specification</a> -->
-              <!-- <a href="#" @click="finishJudge(index,sl.selected)" class="btn btn-outline-primary">심사 완료</a> -->
             </div>
-          </div>
-          <!-- <a href="#" @click="finishJudge" class="btn btn-outline-primary">심사 완료</a> -->
-          <div class="col-12 text-center">
-          <button class="btn btn-primary" @click="finishJudge()">심사 완료</button>
-          </div>
-          </div>
-          <!-- <div > -->
             <v-error v-else-if="$store.state.apply.apply_state==1"></v-error>
-          <!-- </div> -->
-      </div>
+          </div>
+          <div class="col-12 text-center">
+            <button class="btn btn-primary" @click="finishJudge()">심사 완료</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -142,6 +113,10 @@
         await this.getSemester();
       },
       methods: {
+        modalId(i) {
+          console.log(i);
+          return 'modal' + i;
+        },
         ynchange(index,event){
           this.stdList[index].YN = event.target.value;
         },
@@ -161,7 +136,7 @@
               if(response.data =='기간이 없음'){
                   alert('기간이 없습니다.')
                   // this.$router.push({name: "Home"})
-                  
+
               }
               else if(response.data=='공고가 없음'){
                   alert('공고를 작성하지 않으셨습니다.')
@@ -220,6 +195,7 @@
 <style scoped>
 .img {
     height:100px;
+    width:100px;
     border-radius: 50%;
     border-color: coral;
 }
