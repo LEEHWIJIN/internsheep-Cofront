210792eb533ecb0f7d7157e3312ac1864e3d3c92
leehwijin <cysoon96@gmail.com>
2019-06-10 17:18:40 +0900

    edit tag

--- src/components/Apply/modifyNotice.vue
+++ src/components/Apply/modifyNotice.vue
@@ -57,32 +57,16 @@
             <div class="col-lg-12">
               <h6 style="font-weight:bold">태그*</h6>
               <p class="mb-1" style="font-weight:bold; font-size:15px; color:grey;">- 추천 태그 검색</p>
-<!---  -->
              <div class="mb-1">
                <!-- string value -->
-               <model-select :options="options2"
-                             v-model="item2"
-                                       placeholder="태그" @select="selectFromParentComponent2">
-                   {{lastSelect}}
+               <model-select :options="options" v-model="item" placeholder="태그">
                </model-select>
              </div>
-<!--  -->
               <!--<p class="mb-1" style="font-weight:bold; font-size:15px; color:grey;">- 사용자 입력 태그</p>-->
               <!--<input class="form-control mb-4" v-model="cTag" placeholder="태그를 입력 해주세요 ( ex. #프론트엔드개발자 #서버관리 )">-->
               <p class="mb-1" style="font-weight:bold; font-size:15px; color:grey;">- 추가된 태그</p>
               <div class="bg-gray-light mb-3 p-2">
-                <button v-for="(ct,index) in coTag" type="button" class="btn tag-light btn-sm m-1" name="button"> {{ct}}<i class="fa fa-plus" style="font-size:14px;"></i></button>
-                <!--<button onclick="cTagArray('C++')" type="button" class="btn tag-light btn-sm m-1" name="button">C++ &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!--<button onclick="cTagArray('C')" type="button" class="btn tag-light btn-sm m-1" name="button">C &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!--<button onclick="cTagArray('Python')" type="button" class="btn tag-light btn-sm m-1" name="button">Python &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!--<button onclick="cTagArray('Javascript')" type="button" class="btn tag-light btn-sm m-1" name="button">Javascript &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!--<button onclick="cTagArray('Database')" type="button" class="btn tag-light btn-sm m-1" name="button">Database &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!--<button onclick="cTagArray('Mysql')" type="button" class="btn tag-light btn-sm m-1" name="button">Mysql &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!--<button onclick="cTagArray('Django')" type="button" class="btn tag-light btn-sm m-1" name="button">Django &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!--<button onclick="cTagArray('Vue.js')" type="button" class="btn tag-light btn-sm m-1" name="button">Vue.js &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!--<button onclick="cTagArray('HTML/CSS')" type="button" class="btn tag-light btn-sm m-1" name="button">Html/Css &nbsp; &nbsp;<i class="fa fa-plus" style="font-size:14px;"></i></button>-->
-                <!-- <input type="button" value="^_^" id="cat"  onclick="change2(this)" style="background-color:white;"><br>
-클릭하면 바뀌는 버튼<br><br> -->
+                <button v-for="(ct,index) in coTag" type="button" class="btn tag-light btn-sm m-1" @click="removeItem(index)" name="button"> {{ct}}<i class="fa fa-plus" style="font-size:14px;"></i></button>
               </div>
             </div>
             <!-- <h6 style="font-weight:bold">사업자등록번호*</h6>
@@ -106,36 +90,34 @@
 </template>
 
 <script>
-//
 import { ModelSelect } from 'vue-search-select'
-//
 import Const from '../../constant/constant';
-  import VBase from '../Base/Index.vue'
-  import VCategory from '../Category/Index.vue'
-  import DatePicker from 'v-cal-input'
+import VBase from '../Base/Index.vue'
+import VCategory from '../Category/Index.vue'
+import DatePicker from 'v-cal-input'
   export default{
       name: 'Modifyapply',
       data() {
         return {
-          options2: [],
-            coTag : [],
-          item2: '',
+          options: [],
+          coTag : [],
+          item: '',
           cName:[],
           cLocation:[],
           cBenefit:[],
           cPay : [],
-            cManagerPhone :[],
-            cManagerName :[],
-            cInfo : [],
-            cEmail : [],
+          cManagerPhone :[],
+          cManagerName :[],
+          cInfo : [],
+          cEmail : [],
           internTermStart : null,
           cOccupation : [],
           cNumOfPeople : [],
           cTag : [],
           internTermEnd : null,
-           imageData: "",
-           imageURL : null,
-            lastSelect : {},
+          imageData: "",
+          imageURL : null,
+          lastSelect : {},
         }
       },
       components: {
@@ -143,6 +125,21 @@ import Const from '../../constant/constant';
           VCategory,
           DatePicker,
           ModelSelect,
+      },
+      watch:{
+        item : function(){
+          console.log(this.options[this.item].text)
+          for(var i=0;this.coTag.length;i++){
+            if(this.options[this.item].text==this.coTag[i]){
+              console.log(this.coTag[i])  
+              return;
+            } 
+            else break;
+          }
+          console.log("for문 나왔다.")
+          var tagname = this.options[this.item].text;
+          this.coTag.push(tagname)
+        }
       },
       async created(){
         await this.$http.get(Const.API_SERVER+'/',{'headers': {authorization: `Bearer ${localStorage.token}`}}).then(res => {
@@ -154,47 +151,25 @@ import Const from '../../constant/constant';
         await this.loadcoTag()
       },
       methods: {
-        //
-        reset2 () {
-          this.item2 = ''
+        pushTaglist(item2){
+          console.log(item2)
         },
-        selectFromParentComponent2 () {
-          // select option from parent component
-          this.item2 = item2
-          this.lastSelect = lastSelect
-            console.log(this.item2)
-            console.log(this.lastSelect)
+        removeItem(index){
+          this.coTag.splice(index,1)
         },
-          loadTag(){
-              this.$http.get(Const.API_SERVER+'/co/getAllTag').then((response) => {
-                  for(var i =0; i<response.data.length;i++) {
-                      this.options2.push({value: i, text: response.data[i].tag})
-                  }
-              })
-          },
+        loadTag(){
+            this.$http.get(Const.API_SERVER+'/co/getAllTag').then((response) => {
+                for(var i =0; i<response.data.length;i++) {
+                    this.options.push({value: i, text: response.data[i].tag})
+                }
+            })
+        },
         loadcoTag(){
-                this.$http.get(Const.API_SERVER+'/co/getCoTag',{params:{cLoginID:this.user.loginId}}).then((response) => {
-                    for(var i =0; i<response.data.length;i++) {
-                        this.coTag.push(response.data[i].tag)
-                    }
-                })
-        },
-        cTagArray(lang){
-          var overlap = 0; //중복횟수를 저장하는 변수 선언
-
-          for(var i=0; i<this.cTag.length ; i++){
-            if(cTag[i]==lang){  // lang과 같은 태그가 존재할 경우
-              overlap++; //중복횟수를 늘린다
-            }
-          }
-
-          if(overlap>=1){ // 중복횟수가 1이상일 경우
-            return;
-          }
-          else{ // 아닐경우
-            this.cTag.push(lang); // cTag 배열에 저장
-          }
-
+          this.$http.get(Const.API_SERVER+'/co/getCoTag',{params:{cLoginID:this.user.loginId}}).then((response) => {
+              for(var i =0; i<response.data.length;i++) {
+                  this.coTag.push(response.data[i].tag)
+              }
+          })
         },
         previewImage(event) {
           var input = event.target;
@@ -213,9 +188,8 @@ import Const from '../../constant/constant';
             data.append('image', this.imageURL)
             data.append('cBenefit', this.cBenefit)
             data.append('cPay', this.cPay)
-              data.append('internTermStart', this.internTermStart)
-
-              data.append('internTermEnd', this.internTermEnd)
+            data.append('internTermStart', this.internTermStart)
+            data.append('internTermEnd', this.internTermEnd)
             data.append('cOccupation', this.cOccupation)
             data.append('cNumOfPeople',this.cNumOfPeople)
             data.append('cTag',  this.cTag)
@@ -229,6 +203,9 @@ import Const from '../../constant/constant';
                     'Content-Type' : 'multipart/form-data'
                 }
             }
+            // this.$http.post(Const.API_SERVER+'/co/addCoAndTag',{cLoginID:this.user.loginId,tag : this.coTag}).then((response)=>{
+              
+            // })
             this.$http.post(Const.API_SERVER+'/co/mypage/modifyNotice',data,config).then((response) => {
                 alert("수정되었습니다.")
                 this.$store.dispatch('apply/setApplyState',2);
